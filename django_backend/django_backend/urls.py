89b429c34b2ba03ad8384cb45837d9ee4d7cbb73
hd9189 <hzfmd2006@gmail.com>
2023-02-01 10:31:08 -0500

    django

--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -1,4 +1,4 @@
-"""cs_final_project URL Configuration
+"""django_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
